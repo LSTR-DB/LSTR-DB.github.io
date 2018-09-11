126a46706338e348a9c3f14c71e2755685a4ad58
LSTR-DB <43031477+LSTR-DB@users.noreply.github.com>
2018-09-11 11:08:11 +0200

    Add files via upload

---
+++ index-index.js
@@ -0,0 +1,54 @@
+﻿// Trigger CSS animations on scroll.
+// Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
+
+// Looking for a version that also reverses the animation when
+// elements scroll below the fold again?
+// --> Check https://codepen.io/bramus/pen/vKpjNP
+
+jQuery(function($) {
+  
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function() {
+    
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+        $animatables = $('.animatable');
+    
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.length == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+    
+    // Check all animatables and animate them if necessary
+		$animatables.each(function(i) {
+       var $animatable = $(this);
+			if (($animatable.offset().top + $animatable.height() - 0) < offset) {
+        $animatable.removeClass('animatable').addClass('animated');
+			}
+    });
+
+	};
+  
+  // Hook doAnimations on scroll, and trigger a scroll
+	$(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
+
+});
+
+function Expand() {
+    var x = document.getElementById("show");
+    if (x.style.width === "0%") {
+        x.style.width = "100%";
+    } else {
+        x.style.width = "0%";
+    }
+} 
+
+function Retract() {
+    var x = document.getElementById("show");
+    if (x.style.width === "100%") {
+        x.style.width = "0%";
+    } else {
+        x.style.width = "100%";
+    }
+} 
